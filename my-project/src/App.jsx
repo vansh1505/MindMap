e3d62f8737624e777a8877387a531e803c7c60e1
DEVANSH PRATAP SINGH <myivy0412@gmail.com>
2024-11-15 16:44:18 +0530

    Updated Home.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,12 @@
+import Home from "./componets/Home";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div>
+        <h1></h1>
+      </div>
+       <Home/>
+    </>
   )
 }
---
+++ my-project/src/componets/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+return (
+    <div className="min-h-screen bg-gradient-to-r from-slate-50 to-teal-300 flex flex-row justify-center ">
+        <div className="bg-white rounded-3xl p-8 m-3 shadow-[10px_0px_20px_0px_rgba(209,213,219,1)] h-auto w-[calc(100%-320px)]">
+           
+        </div>
+        
+        <div className="preview bg-white rounded-3xl p-8 m-3 my-auto ml-0 shadow-[10px_0px_20px_0px_rgba(209,213,219,1)] h-[300px] w-[320px]">
+           
+        </div>
+    </div>
+);
+}
+
+export default Home;
